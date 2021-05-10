74a1a587667d30671448f1ec0712a7de12d65d44
yingkunm <yingkunm@umich.edu>
2021-05-10 15:09:06 +0800

    3

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const Koa = require('koa')
 const next = require('next')
 const Router = require('koa-router')
-const port = parseInt(process.env.PORT, 10) || 3003
+const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
